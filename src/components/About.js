559166bd415994055fc995e2a67a7613f34f8751
roukell <62136980+roukell@users.noreply.github.com>
2020-10-03 18:52:27 +1000

    added about me carousels

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,74 @@
-import React from 'react';
-import { Container, Col, Image, Row } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Col, Image, Row, Carousel } from 'react-bootstrap';
 
-const About = () => (
-    <>
-        <Container fluid className='aboutContainer'>
-            <div className='aboutDiv'>
-                <Row className="justify-content-md-center">
-                    <Col>
-                        <Image src="holder.js/171x180" roundedCircle />
-                        <p>My photo</p>
-                    </Col>
+function About() {
+    const [index, setIndex] = useState(0);
 
-                    <Col xs={8}>
-                        <h3>About me</h3>
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+    return (
+        <div>
+
+            <Carousel activeIndex={index} onSelect={handleSelect} className='carouselContainer'>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=First slide&bg=373940"
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=Second slide&bg=282c34"
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=Third slide&bg=20232a"
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
                         <p>
-                            I am a full stack web developer mainly using React.
-                    </p>
-                        <p>
-                            type more here
-                    </p>
-                    </Col>
-                </Row>
-            </div>
-        </Container>
-    </>
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
 
-);
+            <Container fluid className='aboutContainer'>
+                <div className='aboutDiv'>
+                    <Row className="justify-content-md-center">
+
+                        <Col xs={8}>
+                            <h3>About me</h3>
+                            <p>
+                                I am a full stack web developer mainly using React.
+                    </p>
+                            <p>
+                                type more here
+                    </p>
+                        </Col>
+                    </Row>
+                </div>
+            </Container>
+
+        </div>
+    )
+};
 
 export default About;
