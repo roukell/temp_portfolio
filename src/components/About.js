2da24b07510708bd1acedec377417183db5d80fb
roukell <62136980+roukell@users.noreply.github.com>
2020-10-05 15:55:53 +1100

    updated projects details

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Col, Row, Carousel, Card } from 'react-bootstrap';
+import { Container, Col, Row, Carousel, Card, Tooltip, OverlayTrigger } from 'react-bootstrap';
 
 function About() {
     const [index, setIndex] = useState(0);
@@ -7,6 +7,7 @@ function About() {
     const handleSelect = (selectedIndex, e) => {
         setIndex(selectedIndex);
     };
+
     return (
         <div>
             <Carousel activeIndex={index} onSelect={handleSelect} className='carouselContainer'>
@@ -44,88 +45,126 @@ function About() {
                         <h3>Third slide label</h3>
                         <p>
                             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+                        </p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
 
             <Container className='aboutContainer'>
-            <Row>
-            <Col></Col>
-            <Col xs={12}>
- <Card>
-     <Card.Body>
-                            <h3>Learn more about Melody</h3>
-                            <p>
-                                Melody is a Full Stack Web Developer experience in designing and developing web applications by using React. She always pay high attention to detail and have created several functional websites with her proven problem solving skills.
-<br />
+                <Row>
+                    <Col></Col>
+                    <Col xs={12}>
+                        <Card>
+                            <Card.Body>
+                                <h3>Learn more about Melody</h3>
+                                <p>
+                                    Melody is a Full Stack Web Developer experience in designing and developing web applications by using React. She always pay high attention to detail and have created several functional websites with her proven problem solving skills.
+                                    <br />
+                                    <br />
+                                    She has strong time management skills with the ability to prioritise workload in order to meet strict deadlines. Melody is also experienced in communicating and maintaining relationships with key internal and external stakeholders.
+                                    <br />
+                                    <br />
+                                    Melody is highly motivated to develop interactive websites that enhances the user experience by providing a clean, simple and intuitive interface. She has recently developed an application to solve employee directory problems.
+                                    <br />
+                                    <br />
+                                    Her goal to develop creative and functional website experiences for people, utilising latest technologies and innovative problem solving mindset.
+                                    <br />
+                                    <br />
+                                    When she is not coding...
+                                    <br />
+                                    She enjoys traveling, cooking, diving, and doing weight training.
+                                    <br />
+                                    She loves animation, digital and graphic design, and aquarium.  
+                                    <br />
+                                    <br />
+                                    Thank you for your time! Feel free to connect her on Linkedin! 
+                                </p>
                                 <br />
-She has strong time management skills with the ability to prioritise workload in order to meet strict deadlines. Melody is also experienced in communicating and maintaining relationships with key internal and external stakeholders.
-<br />
-                                <br />
-She is highly motivated to develop interactive websites that enhances the user experience by providing a clean, simple and intuitive interface. Melody has recently developed an application to solve employee directory problems.
-<br />
-                                <br />
-Her goal to develop creative and functional website experiences for people, utilising latest technologies and innovative problem solving mindset.
-                            </p>
-                            <br />
-                            <ul class="list-inline dev-icons">
-                            <li class="list-inline-item">
-                            <a href="https://github.com/roukell" target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/nolan/64/github.png" alt='github'/></a>
-                            </li>
-                            <li class="list-inline-item">
-                            <a href="https://www.linkedin.com/in/yilingmelodylo/" target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/cute-clipart/64/000000/linkedin.png" alt='linkedin'/></a>
-                            </li>
-                            <li class="list-inline-item">
-                            <a href="mailto:yiling.melody.lo@gmail.com" target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/cute-clipart/64/000000/email.png" alt='email'/></a>
-                            </li>       
-                            </ul>
+                                <ul class="list-inline dev-icons">
+                                    <li class="list-inline-item">
+                                        <OverlayTrigger
+                                            placement="top"
+                                            delay={{ show: 250, hide: 400 }}
+                                            overlay={<Tooltip id="button-tooltip-2">Check out my GitHub</Tooltip>}
+                                        >
+                                            <a href="https://github.com/roukell" target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/nolan/64/github.png" alt='github' /></a>
+                                        </OverlayTrigger>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <OverlayTrigger
+                                            placement="top"
+                                            delay={{ show: 250, hide: 400 }}
+                                            overlay={<Tooltip id="button-tooltip-2">Check out my LinkedIn</Tooltip>}
+                                        >
+                                            <a href="https://www.linkedin.com/in/yilingmelodylo/" target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/cute-clipart/64/000000/linkedin.png" alt='linkedin' /></a>
+                                        </OverlayTrigger>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <OverlayTrigger
+                                            placement="top"
+                                            delay={{ show: 250, hide: 400 }}
+                                            overlay={<Tooltip id="button-tooltip-2">Send me an email</Tooltip>}
+                                        >
+                                            <a href="mailto:yiling.melody.lo@gmail.com" target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/cute-clipart/64/000000/email.png" alt='email' /></a>
+                                        </OverlayTrigger>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <OverlayTrigger
+                                            placement="top"
+                                            delay={{ show: 250, hide: 400 }}
+                                            overlay={<Tooltip id="button-tooltip-2">View my resume</Tooltip>}
+                                        >
+                                            <a href="https://docs.google.com/document/d/1YKEJuD0zNYsoyLNUBiFoy4sHzG5fs56CXmOLfGj6l8w/edit?usp=sharing" target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/office/64/000000/open-resume.png" alt='resume' /></a>
+                                        </OverlayTrigger>
+                                    </li>
+                                </ul>
 
-                            <section class="resume-section" id="skills">
-                                <div class="resume-section-content">
-                                    <h3 class="subheading mb-3">Programming Languages & Tools</h3>
-                                    <ul class="list-inline dev-icons">
-                                        <li class="list-inline-item">
-                                            <img src="https://img.icons8.com/color/48/000000/html-5.png" alt='html5'/></li>
-                                        <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/css3.png" alt='css3'/></li>
-                                        <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/javascript.png" alt='javascript'/></li>
-                                        <li class="list-inline-item"><img src="https://img.icons8.com/ios-filled/50/000000/jquery.png" alt='jquery'/></li>
-                                        <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/react-native.png" alt='react'/></li>
-                                        <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/nodejs.png" alt='nodejs'/></li>
-                                        <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/bootstrap.png" alt='bootstrap'/></li>
-                                        <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/npm.png" alt='npm'/></li>
-                                        <li class="list-inline-item"><img src="https://img.icons8.com/ios-filled/50/000000/mysql-logo.png" alt='mysql'/></li>
-                                        <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/mongodb.png" alt='mongodb'/></li>
-                                        <li class="list-inline-item"><img src="https://img.icons8.com/nolan/64/api-settings.png" alt='api'/></li>
+                                <section class="resume-section" id="skills">
+                                    <div class="resume-section-content">
+                                        <h3 class="subheading mb-3">Programming Languages & Tools</h3>
+                                        <ul class="list-inline dev-icons">
+                                            <li class="list-inline-item">
+                                                <img src="https://img.icons8.com/color/48/000000/html-5.png" alt='html5' /></li>
+                                            <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/css3.png" alt='css3' /></li>
+                                            <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/javascript.png" alt='javascript' /></li>
+                                            <li class="list-inline-item"><img src="https://img.icons8.com/ios-filled/50/000000/jquery.png" alt='jquery' /></li>
+                                            <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/react-native.png" alt='react' /></li>
+                                            <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/nodejs.png" alt='nodejs' /></li>
+                                            <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/bootstrap.png" alt='bootstrap' /></li>
+                                            <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/npm.png" alt='npm' /></li>
+                                            <li class="list-inline-item"><img src="https://img.icons8.com/ios-filled/50/000000/mysql-logo.png" alt='mysql' /></li>
+                                            <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/mongodb.png" alt='mongodb' /></li>
+                                            <li class="list-inline-item"><img src="https://img.icons8.com/nolan/64/api-settings.png" alt='api' /></li>
+                                            <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/json--v1.png" alt='json' /></li>
+                                            <li class="list-inline-item"><img src="https://img.icons8.com/color/48/000000/heroku.png" alt='heroku' /></li>
+                                        </ul>
 
-                                    </ul>
-            
-                                    <ul class="fa-ul mb-0">
-                                        <li>
-                                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                        Mobile-First, Responsive Design
-                    </li>
-                                        <li>
-                                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                        Cross Browser Testing & Debugging
-                    </li>
-                                        <li>
-                                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                        Cross Functional Teams
-                    </li>
-                                        <li>
-                                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                        Relational and Non-relational Database
-                    </li>
-                                    </ul>
-                                </div>
-                            </section>
-            </Card.Body>
-</Card>
-</Col>
-<Col></Col>
-</Row>
-</Container>
+                                        <ul class="fa-ul mb-0">
+                                            <li>
+                                                <span class="fa-li"><i class="fas fa-check"></i></span>
+                                                Mobile-First, Responsive Design
+                                            </li>
+                                            <li>
+                                                <span class="fa-li"><i class="fas fa-check"></i></span>
+                                                Cross Browser Testing & Debugging
+                                            </li>
+                                            <li>
+                                                <span class="fa-li"><i class="fas fa-check"></i></span>
+                                                Cross Functional Teams
+                                            </li>
+                                            <li>
+                                                <span class="fa-li"><i class="fas fa-check"></i></span>
+                                                Relational and Non-relational Database
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </section>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col></Col>
+                </Row>
+            </Container>
         </div>
     )
 };
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import { CardDeck, Card, Row, Col } from 'react-bootstrap';
-import movieManagerImg from '../img/movie_manager.png';
-import vBloggerImg from '../img/v_bloggers_around_you.png';
+import movieManagerImg from '../img/movie_manager_screenshot.png';
+import vBloggerImg from '../img/v_bloggers_screenshot.png';
+import employeeDirectoryImg from '../img/employee_directory_screenshot.png';
 import burgerLoggerImg from '../img/burger_logger.png';
-import weatherDashImg from '../img/weather_dashboard_logo.png';
+import weatherDashImg from '../img/weather_dash_board_screenshot.png';
+import onlnieQuizImg from '../img/online_quiz_screenshot.png'
+
 
 function Project() {
 
     return (
         <div className='projectDiv'>
             <h1 className='projectTitle'>Projects</h1>
-
             <CardDeck className="justify-content-md-center">
                 <Row xs={1} md={3} >
                     <Col>
                         <Card border="light">
                             <Card.Img variant="top" src={movieManagerImg} className='projectImg'/>
                             <Card.Body>
-                                <Card.Title>Group Project 2</Card.Title>
+                                <Card.Title>Want to Watch Movie Manager</Card.Title>
                                 <Card.Text>
-                                    put project here
+                                Want to Watch Movie Manager is a website which allows users to search movies available in the market and keep track of them.
                     </Card.Text>
-                                <Card.Link href="#">Card Link</Card.Link>
-                                <Card.Link href="#">Another Link</Card.Link>
+                                <Card.Link href="https://github.com/roukell/Want-to-Watch-Series-Manager" target="_blank" rel="noopener noreferrer">GitHub</Card.Link>
+                                <Card.Link href="https://damp-dusk-40026.herokuapp.com/" target="_blank" rel="noopener noreferrer">Deployed Application</Card.Link>
                             </Card.Body>
                            
                         </Card>
@@ -32,27 +34,26 @@ function Project() {
                         <Card border="light">
                             <Card.Img variant="top" src={vBloggerImg} className='projectImg'/>
                             <Card.Body>
-                                <Card.Title>Group Project 1</Card.Title>
+                                <Card.Title>V-Bloggers Around You</Card.Title>
                                 <Card.Text>
-                                    Type here
+                                Find v-bloggers around your local area to help promoting your business
                         </Card.Text>
-                                <Card.Link href="#">Card Link</Card.Link>
-                                <Card.Link href="#">Another Link</Card.Link>
+                                <Card.Link href="https://github.com/roukell/vbloggers_around_you" target="_blank" rel="noopener noreferrer">GitHub</Card.Link>
+                                <Card.Link href="https://roukell.github.io/vbloggers_around_you/" target="_blank" rel="noopener noreferrer">Deployed Application</Card.Link>
                             </Card.Body>
-                         
                         </Card>
                     </Col>
 
                     <Col>
                         <Card border="light">
-                            <Card.Img variant="top" src={burgerLoggerImg} className='projectImg'/>
+                            <Card.Img variant="top" src={employeeDirectoryImg} className='projectImg'/>
                             <Card.Body>
-                                <Card.Title>Project 1</Card.Title>
+                                <Card.Title>Employee Directory</Card.Title>
                                 <Card.Text>
-                                    put project here
+                                Employee Directory assists employers to view their entire employee directory at once and provides quick access to employee information.
                     </Card.Text>
-                                <Card.Link href="#">Card Link</Card.Link>
-                                <Card.Link href="#">Another Link</Card.Link>
+                                <Card.Link href="https://github.com/roukell/employee_directory" target="_blank" rel="noopener noreferrer">GitHub</Card.Link>
+                                <Card.Link href="https://roukell.github.io/employee_directory/" target="_blank" rel="noopener noreferrer">Deployed Application</Card.Link>
                             </Card.Body>
                            
                         </Card>
@@ -66,14 +67,14 @@ function Project() {
                 <Row xs={1} md={3} >
                     <Col>
                         <Card border="light">
-                            <Card.Img variant="top" src={weatherDashImg} className='projectImg'/>
+                            <Card.Img variant="top" src={burgerLoggerImg} className='projectImg'/>
                             <Card.Body>
-                                <Card.Title>Project 1</Card.Title>
+                                <Card.Title>Burger Logger</Card.Title>
                                 <Card.Text>
-                                    put project here
+                                Burger Logger is a restaurant app that allow users to enter their favourite burger name.
                     </Card.Text>
-                                <Card.Link href="#">Card Link</Card.Link>
-                                <Card.Link href="#">Another Link</Card.Link>
+                    <Card.Link href="https://github.com/roukell/burger_logger" target="_blank" rel="noopener noreferrer">GitHub</Card.Link>
+                                <Card.Link href="https://serene-hamlet-38122.herokuapp.com/" target="_blank" rel="noopener noreferrer">Deployed Application</Card.Link>
                             </Card.Body>
                            
                         </Card>
@@ -81,14 +82,14 @@ function Project() {
 
                     <Col>
                         <Card border="light">
-                            <Card.Img variant="top" src={vBloggerImg} className='projectImg'/>
+                            <Card.Img variant="top" src={weatherDashImg} className='projectImg'/>
                             <Card.Body>
-                                <Card.Title>Card title</Card.Title>
+                                <Card.Title>Weather Dashboard</Card.Title>
                                 <Card.Text>
-                                    Type here
+                                This is a weather dashboard including forecast for the day and the future 5 days.
                         </Card.Text>
-                                <Card.Link href="#">Card Link</Card.Link>
-                                <Card.Link href="#">Another Link</Card.Link>
+                        <Card.Link href="https://github.com/roukell/weather_dashboard" target="_blank" rel="noopener noreferrer">GitHub</Card.Link>
+                                <Card.Link href="https://roukell.github.io/weather_dashboard/" target="_blank" rel="noopener noreferrer">Deployed Application</Card.Link>
                             </Card.Body>
                          
                         </Card>
@@ -96,14 +97,14 @@ function Project() {
 
                     <Col>
                         <Card border="light">
-                            <Card.Img variant="top" src={movieManagerImg} className='projectImg'/>
+                            <Card.Img variant="top" src={onlnieQuizImg} className='projectImg'/>
                             <Card.Body>
-                                <Card.Title>Project 1</Card.Title>
+                                <Card.Title>Online Quiz</Card.Title>
                                 <Card.Text>
-                                    put project here
+                                Test your HTML, CSS, Javascript knowledge
                     </Card.Text>
-                                <Card.Link href="#">Card Link</Card.Link>
-                                <Card.Link href="#">Another Link</Card.Link>
+                    <Card.Link href="https://github.com/roukell/online_quiz" target="_blank" rel="noopener noreferrer">GitHub</Card.Link>
+                                <Card.Link href="https://roukell.github.io/online_quiz/" target="_blank" rel="noopener noreferrer">Deployed Application</Card.Link>
                             </Card.Body>
                            
                         </Card>
